ac67654a3d7961fda7bea21ad7fa2cebe534f8b2
Harshu200502 <harshitha200502@gmail.com>
2026-05-26 19:08:10 +0530

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+const char* WIFI_SSID = "ESP32";
+const char* WIFI_PASSWORD = "12345678";
+
+// ThingSpeak (optional)
+const char* THINGSPEAK_API_KEY = "YOUR_API_KEY";
+const char* THINGSPEAK_URL = "http://api.thingspeak.com/update";
+
+#endif
